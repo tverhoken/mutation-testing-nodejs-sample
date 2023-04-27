989cd3642a33796666e5faace26b53a00181c0fa
tverhoken <tverhoken@norsys.fr>
2023-04-27 06:04:48 +0200

    Refactor REST controller

--- src/rest-controller.js
+++ src/rest-controller.js
@@ -1,26 +1,45 @@
 import { searchByEmail, searchByIdentity } from './search';
 
 export default function main({ query }) {
-    const queryParams = Object.keys(query);
+    return Promise.resolve(Object.keys(query))
+        .then(hasQueryParams)
+        .then(checkQueryParams(query))
+        .then(searchUsers(query))
+        .then(data => ({ status: 200, body: { data } }))
+        .catch(reason => ({ status: 400, body: { error: reason } }));
+
+}
+
+function hasQueryParams(queryParams) {
     if (queryParams.length) {
-        if (query.email) {
-            return searchByEmail(query.email)
-                .then(data => ({ status: 200, body: { data } }));
+        return queryParams;
+    }
+    return Promise.reject('No query parameter provided.');
+}
+
+function checkQueryParams(query) {
+    return queryParams => {
+        if (query.email || (query.firstname && query.lastname)) {
+            return Promise.resolve();
         }
         const hasEmailParam = queryParams.some(param => param === 'email');
         if (hasEmailParam) {
-            return Promise.resolve({ status: 400, body: { error: 'email param provided with empty value.' } });
-        }
-        if (query.firstname && query.lastname) {
-            return searchByIdentity(query.firstname, query.lastname)
-                .then(data => ({ status: 200, body: { data } }));
+            return Promise.reject('email param provided with empty value.');
         }
         const hasLastnameParam = queryParams.some(param => param === 'lastname');
         const hasFirstnameParam = queryParams.some(param => param === 'firstname');
         if (!hasLastnameParam || !hasFirstnameParam) {
-            return Promise.resolve({ status: 400, body: { error: 'Missing required search parameter.' } });
+            return Promise.reject('Missing required search parameter.');
         }
-        return Promise.resolve({ status: 400, body: { error: 'lastname and firstname params provided with empty value.' } });
+        return Promise.reject('lastname and firstname params provided with empty value.');
     }
-    return Promise.resolve({ status: 400, body: { error: 'No query parameter provided.' } });
+}
+
+function searchUsers({ email, firstname, lastname }) {
+    return () => {
+        if (email) {
+            return searchByEmail(email);
+        }
+        return searchByIdentity(firstname, lastname);
+    };
 }
